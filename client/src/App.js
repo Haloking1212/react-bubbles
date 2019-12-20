6410a857047c105e1b60133528fc234e9239b5e0
Hector Valdez <hvaldez180@gmail.com>
2019-12-20 13:05:10 -0500

    completed step 1 and partial step 2

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,33 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 import Login from "./components/Login";
 import "./styles.scss";
 
 function App() {
+  let password = "i<3Lambd4"
+
   return (
-    <Router>
+    <div className="App">
+        <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        {/* 
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/protected">Protected Page</Link>
+          </li>
+        </ul>
+        <Switch>
+          <PrivateRoute exact path="/colors" component={BubblePage} />
+          <Route path="/login" component={Login} />
+          <Route component={Login} />
+        </Switch>
       </div>
     </Router>
+    </div>
   );
 }
 
